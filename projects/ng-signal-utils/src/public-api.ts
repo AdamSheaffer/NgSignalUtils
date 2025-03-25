6ef474954b5f4b93db44d65bde21c94466d12021
Adam Sheaffer <adam.sheaffer@tn.gov>
2025-03-24 22:42:14 -0500

    effectWithDeps

--- projects/ng-signal-utils/src/public-api.ts
+++ projects/ng-signal-utils/src/public-api.ts
@@ -2,5 +2,6 @@
  * Public API Surface of ng-signal-utils
  */
 
+export * from './lib/effect-with-deps';
 export * from './lib/local-storage-signal';
-export * from './lib/sessionStorageSignal';
+export * from './lib/session-storage-signal';
