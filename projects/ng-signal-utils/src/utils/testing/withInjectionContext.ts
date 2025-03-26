ac355d704a5eda3ca6a0e4c39edb3298369791e1
Adam Sheaffer <adam.sheaffer@tn.gov>
2025-03-26 11:30:25 -0500

    remove unnecessary docstring on utility function

--- projects/ng-signal-utils/src/utils/testing/withInjectionContext.ts
+++ projects/ng-signal-utils/src/utils/testing/withInjectionContext.ts
@@ -1,14 +1,5 @@
 import { Injector, runInInjectionContext } from '@angular/core';
 
-/**
- * Runs a function within an Angular injection context.
- *
- * @template TFunc The type of function to be wrapped
- * @param injector The Angular injector to use for the context
- * @param fn The function to run within the injection context
- * @returns A function with the same signature as the input function which runs in the provided injection context
- *
- */
 export function withInjectionContext<TFunc extends (...args: any[]) => any>(
   injector: Injector,
   fn: TFunc
