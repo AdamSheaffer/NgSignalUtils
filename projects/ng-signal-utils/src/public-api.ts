3bc782f8582335f3164238e71a18e38b958bf7cd
Adam Sheaffer <adam.sheaffer@tn.gov>
2025-03-26 14:01:40 -0500

    add missing export

--- projects/ng-signal-utils/src/public-api.ts
+++ projects/ng-signal-utils/src/public-api.ts
@@ -7,5 +7,6 @@ export * from './lib/debounced-signal';
 export * from './lib/effect-until';
 export * from './lib/effect-with-deps';
 export * from './lib/local-storage-signal';
+export * from './lib/managed-signal-history';
 export * from './lib/session-storage-signal';
 export * from './lib/signal-history';
